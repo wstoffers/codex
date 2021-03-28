5152275645bf21cb1579a1908558ae8a5b37dfbd
Will S <73315256+wstoffers@users.noreply.github.com>
2021-03-28 13:37:28 -0600

    Adding warning

--- src/preprocess/runner.py
+++ src/preprocess/runner.py
@@ -99,5 +99,6 @@ if __name__ == '__main__':
             other.append(quantity)
         else:
             etoh.append(quantity)
-    print(f'{os.linesep}EtOH: {sum(etoh)} (automatic label based on section)')
+    print(f'{os.linesep}**WARNING** NA does not literally mean zero-proof!')
+    print(f'EtOH: {sum(etoh)} (automatic label based on section)')
     print(f'{os.linesep}Total: {sum(etoh+other)} ({sum(other)} other)')
