2cff13c71cc00156d39351a542c7e84e5063e9e3
Will S <73315256+wstoffers@users.noreply.github.com>
2021-03-29 00:45:44 -0600

    Adding code which is functional for first column only

--- src/preprocess/pdfExtraction.py
+++ src/preprocess/pdfExtraction.py
@@ -7,18 +7,55 @@ import codecs
 import pdfplumber
 
 #define:
-def pdfExperiment(filePath):
-    with pdfplumber.open(filePath) as pdf:
-        for page in pdf.pages:
-            if page.page_number == 7:
-                words = page.extract_words(use_text_flow=True)
-                everything = page.extract_text()
-                break
-    directory = os.path.dirname(filePath)
-    with open(os.path.join(directory,'words.plumbing'),'w') as wordsLog:
-        wordsLog.write(os.linesep.join([w['text'] for w in words]))
-    with open(os.path.join(directory,'text.plumbing'),'w') as textLog:
-        textLog.write(everything)
+class unscrambler(object):
+    def __init__(self, filePath):
+        self.filePath = filePath
+
+    def overcomeColumnsObstacle(self):
+        words, lines = self.extract()
+        cursor = 0
+        setInStone = []
+        with open(os.path.join(os.path.dirname(self.filePath),
+                               'test.ing'),'w') as log:
+            processAgain = []
+            for line in lines:
+                keep = []
+                linePieces = line.split()
+                batchSize = len(linePieces)
+                batch = words[cursor:cursor+batchSize]
+                orphan, batchOffset = '', 0
+                for i, piece in enumerate(linePieces):
+                    if piece == batch[i-batchOffset]:
+                        keep.append(piece)
+                    else:
+                        if orphan:
+                            reunited = orphan + piece
+                            if reunited == batch[i-batchOffset]:
+                                keep.append(reunited)
+                                orphan = ''
+                            else:
+                                setInStone.append(keep)
+                                batchOffset = 0
+                                cursor += len(keep)
+                                processAgain.append(' '.join(linePieces[i-1:]))
+                                break
+                        else:
+                            orphan = piece
+                            batchOffset += 1
+            for thing in setInStone:
+                log.write(f'{thing}' + os.linesep)
+            log.write(f'{lines}' + os.linesep)
+        return setInStone
+    
+    def extract(self):
+        with pdfplumber.open(self.filePath) as pdf:
+            for page in pdf.pages:
+                if page.page_number == 7:
+                    words = page.extract_words(use_text_flow=True)
+                    everything = page.extract_text()
+                    break
+        return [w['text'] for w in words], everything.split(os.linesep)
+                
 #run:
 if __name__ == '__main__':
     import argparse
@@ -26,4 +63,4 @@ if __name__ == '__main__':
     parser.add_argument('--file', '-f', required=True,
                         help='path to pdf')
     args = parser.parse_args()
-    pdfExperiment(args.file)
+    final = unscrambler(args.file).overcomeColumnsObstacle()
