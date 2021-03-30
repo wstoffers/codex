5053d3b8ed2e8ffd5aed6e0edc14648bd673bf47
Will S <73315256+wstoffers@users.noreply.github.com>
2021-03-29 23:35:24 -0600

    Adding Pytest module for complicated, low-level code

---
+++ src/preprocess/test_pdfExtraction.py
@@ -0,0 +1,41 @@
+#**    This line is 79 characters long.  The 80th character should wrap.   ***\
+
+#imports:
+import re
+import pytest
+from pdfExtraction import unscrambler
+
+#define:
+@pytest.fixture
+def modifiedUnscrambler(args):
+    '''Returns an unscrambler object, modified for testing'''
+    testObject = unscrambler('.')
+    testObject.extract = lambda k: [args[0], args[1]]
+    return testObject
+
+def testDataSimpleTwoColumns():
+    lines = ['this is a list from two columns']
+    words = ['this', 'is', 'a', 'list', 'of', 'strings', 'in', 'order']    
+    return [[words, lines]]
+
+@pytest.mark.parametrize('args', testDataSimpleTwoColumns())
+def test_overcomeColumnsObstacle(modifiedUnscrambler):
+    assert modifiedUnscrambler.overcomeColumnsObstacle() == [['this',
+                                                              'is',
+                                                              'a',
+                                                              'list']]
+
+def testDataOrphanedString():
+    lines = ['this l ong list from two columns']
+    words = ['this', 'long', 'list', 'of', 'strings']
+    return [[words, lines]]
+
+@pytest.mark.parametrize('args', testDataOrphanedString())
+def test_overcomeColumnsObstacle(modifiedUnscrambler):
+    assert modifiedUnscrambler.overcomeColumnsObstacle() == [['this',
+                                                              'long',
+                                                              'list']]
+
+#run:
+if __name__ == '__main__':
+    pass
