00c19bebd56883bfcff2286b7568e41dbd398700
Will S <73315256+wstoffers@users.noreply.github.com>
2021-03-30 13:08:02 -0600

    Adding PDF extraction before removal of orphan processing

--- src/preprocess/pdfExtraction.py
+++ src/preprocess/pdfExtraction.py
@@ -10,43 +10,57 @@ import pdfplumber
 class unscrambler(object):
     def __init__(self, filePath):
         self.filePath = filePath
+        self.cursor = 0
+        self.words = None
+        self.setInStone = []
 
     def overcomeColumnsObstacle(self):
-        words, lines = self.extract()
-        cursor = 0
-        setInStone = []
+        self.words, lines = self.extract()
         with open(os.path.join(os.path.dirname(self.filePath),
                                'test.ing'),'w') as log:
-            processAgain = []
-            for line in lines:
-                keep = []
-                linePieces = line.split()
-                batchSize = len(linePieces)
-                batch = words[cursor:cursor+batchSize]
-                orphan, batchOffset = '', 0
-                for i, piece in enumerate(linePieces):
-                    if piece == batch[i-batchOffset]:
-                        keep.append(piece)
-                    else:
-                        if orphan:
-                            reunited = orphan + piece
-                            if reunited == batch[i-batchOffset]:
-                                keep.append(reunited)
-                                orphan = ''
-                            else:
-                                setInStone.append(keep)
-                                batchOffset = 0
-                                cursor += len(keep)
-                                processAgain.append(' '.join(linePieces[i-1:]))
-                                break
+            self.unscramble(lines, log)
+        return self.setInStone
+
+    def unscramble(self, lines, log):
+        processAgain = []
+        for line in lines:
+            keep = []
+            linePieces = line.split()
+            batchSize = len(linePieces)
+            batch = self.words[self.cursor:self.cursor+batchSize]
+            log.write(f'    linePieces: {linePieces}{os.linesep}')
+            log.write(f'    batch: {batch}{os.linesep}')
+            orphan, batchOffset = '', 0
+            for i, piece in enumerate(linePieces):
+                if piece == batch[i-batchOffset]:
+                    keep.append(piece)
+                else:
+                    if orphan:
+                        reunited = orphan + piece
+                        if reunited == batch[i-batchOffset]:
+                            keep.append(reunited)
+                            orphan = ''
                         else:
-                            orphan = piece
-                            batchOffset += 1
-            for thing in setInStone:
-                log.write(f'{thing}' + os.linesep)
-            log.write(f'{lines}' + os.linesep)
-        return setInStone
-    
+                            self.setInStone.append(keep)
+                            batchOffset = 0
+                            self.cursor += len(keep)
+                            log.write(f'    orphan: {orphan}{os.linesep}')
+                            log.write(f'    kept: {keep}{os.linesep}')
+                            stillNeeded = ' '.join(linePieces[i-1:])
+                            log.write(f'stillNeeded: {stillNeeded}{os.linesep}')
+                            processAgain.append(stillNeeded)
+                            break
+                    else:
+                        orphan = piece
+                        batchOffset += 1
+        log.write(f'{len(processAgain)} (orphan is {repr(orphan)}): {os.linesep}')
+#        for thing in lines:
+#            log.write(f'    line: {thing}{os.linesep}')
+        self.unscramble(processAgain, log)
+        for thing in self.setInStone:
+            log.write(f'{thing}' + os.linesep)
+        log.write(f'{lines}' + os.linesep)
+
     def extract(self):
         with pdfplumber.open(self.filePath) as pdf:
             for page in pdf.pages:
