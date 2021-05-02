4704c693e045bc36a55e5bc6f796cb3de64fda3d
Will S <73315256+wstoffers@users.noreply.github.com>
2021-05-01 23:39:15 -0600

    Adding MacBook files to prepare for Flutter web merge

--- src/preprocess/extractFromWord.py
+++ src/preprocess/extractFromWord.py
@@ -17,7 +17,7 @@ class extractor(object):
             boldRuns = self.findBold(paragraph)
             if boldRuns:
                 recipeStart = True
-                keep.append(boldRuns)
+                keep.append(['**'] + boldRuns + ['**'])
             else:
                 keep.append([paragraph.text])
                 recipeStart = False
@@ -27,12 +27,43 @@ class extractor(object):
         bold = []
         for run in paragraph.runs:
             if run.bold:
-                bold.append(f'**bold**{run.text}')
+                bold.append(f'{run.text}')
         return bold
 
-def reformat(agglomerateString):
-    reformatted = agglomerateString
-    return reformatted
+class recipe(object):
+    def __init__(self, title):
+        self.title = title
+        self.credit = None
+        self.spec = []
+        self.build = []
+
+def separate(lines):
+    recipes = []
+    bold = re.compile('^\*\*(.+)\*\*$')
+    for line in lines:
+        match = bold.match(line)
+        if match:
+            try:
+                recipes.append(currentRecipe)
+            except NameError:
+                assert len(recipes) == 0
+                #must be first recipe
+            currentRecipe = recipe(match.group(1))
+            build = False
+        else:
+            if currentRecipe.credit:
+                if line.strip():
+                    if build:
+                        currentRecipe.build.append(line)
+                    else:
+                        currentRecipe.spec.append(line)
+                else:
+                    if currentRecipe.spec:
+                        build = True
+            else:
+                currentRecipe.credit = line
+    recipes.append(currentRecipe)
+    return recipes
                 
 #run:
 if __name__ == '__main__':
@@ -41,7 +72,13 @@ if __name__ == '__main__':
     parser.add_argument('--file', '-f', required=True,
                         help='path to word doc')
     args = parser.parse_args()
-    extracted = extractor(args.file).extract()
+    lineParts = extractor(args.file).extract()
+    lines = [''.join(x) for x in lineParts]
+    recipes = separate([x.replace(' ','') for x in lines])
     with open(os.path.join(os.path.dirname(args.file),
-                           'extractedFromWord.log'),'w') as extraction:
-        extraction.write(os.linesep.join([''.join(x) for x in extracted]))
+                           'extractedFromWord.log'),'w') as csv:
+        csv.write(('\n'*2).join([f'{r.title}{os.linesep}'
+                                 f'{r.credit}{os.linesep}'
+                                 f'{os.linesep.join(r.spec)}{os.linesep}'
+                                 f'    '
+                                 f'{"    ".join(r.build)}' for r in recipes]))
